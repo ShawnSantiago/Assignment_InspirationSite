67173320503f8e2bda3ad2b50119b600d27b887c
ShawnSantiago <santiagoshawn@gmail.com>
2014-11-12 12:51:33 -0500

    1.4

--- js/setting.js
+++ js/setting.js
@@ -1,22 +1,25 @@
+// Json Options
+
 var callback = function (listDetails) {
         var html = '';
 
         listDetails.shots.forEach(function (shot) {
-            html += '<li><h3>' + shot.title + '</h3>';
+            html += '<li><div id="header"><h3>' + shot.title + '</h3>';
             html += '<h4>by ' + shot.player.name + '</h4><a href=' + shot.image_url + ' class="popup">';
-            html += '<img src="' + shot.image_teaser_url + '" ';
+            html += '</div><img src="' + shot.image_teaser_url + '" ';
             html += 'id="' + shot.id + '" ';
 
             html += 'alt="' + shot.title + ';"></a>';
             html += '</div>';
             html += '<div id="checkboxSection">';
-            html += '<input type="checkbox" id="1" name="cc" /><label for="1"><span></span></label>';
-            html += '<input type="checkbox" id="2" name="cc" /><label for="2"><span></span></label>';
-            html += '<input type="checkbox" id="3" name="cc" /><label for="3"><span></span></label>';
-            html += '<input type="checkbox" id="4" name="cc" /><label for="4"><span></span></label>';
-            html += '<input type="checkbox" id="5" name="cc" /><label for="5"><span></span></label>';
+            html += '<input type="checkbox" class="checkbox" id="'+ shot.id +'_1" name="'+ shot.id +'" /><label for="'+ shot.id +'_1"><span></span></label>';
+            html += '<input type="checkbox" class="checkbox" id="'+ shot.id +'_2" name="'+ shot.id +'" /><label for="'+ shot.id +'_2"><span></span></label>';
+            html += '<input type="checkbox" class="checkbox" id="'+ shot.id +'_3" name="'+ shot.id +'" /><label for="'+ shot.id +'_3"><span></span></label>';
+            html += '<input type="checkbox" class="checkbox" id="'+ shot.id +'_4" name="'+ shot.id +'" /><label for="'+ shot.id +'_4"><span></span></label>';
+            html += '<input type="checkbox" class="checkbox" id="'+ shot.id +'_5" name="'+ shot.id +'" /><label for="'+ shot.id +'_5"><span></span></label>';
             html +='</div>';
             html += '<input type="text" id="commenttext" placeholder="Leave a comment">';
+            html += '<input type="submit" id="submit" placeholder="submit">';
             html +='</li>';
         });
 
@@ -24,5 +27,26 @@ var callback = function (listDetails) {
     }
 
     jribbble.getShotsByList('popular', callback, {page: 1, per_page: 50});
+
+// function checkCheckBoxes(){
+//     for (var i = 0; i < 5; i++) {
+//         var checkbox = document.getElementByClassName('checkbox');
+//         var checkboxid = checkbox.name;
+//         
+//     }
+// }
+
+function deleteCheckBox() {
+    var div = document.getElementById('checkboxSection');
+    var child = ul.children;
+    for (var i=0; i < div.length; i++) {
+        if (checkbox.checkbox && checkbox.id == 5 ) {
+
+        }
+    }
+}
+
+
+//Pop Up Options
 
 $('a.popup').popup();
